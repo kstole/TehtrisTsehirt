f11e068df8f7c89067f1c449eae6a2fbabfb5aab
Kyler Stole <stolek@onid.oregonstate.edu>
2015-04-10 23:25:16 -0700

    Created Tetris game struct with initialization and fillArray functions and function to check for game loss.

--- TehtrisTsehirt/main.c
+++ TehtrisTsehirt/main.c
@@ -7,9 +7,59 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+typedef struct Tetris {
+    u_int8_t** board;
+    u_int8_t height;
+    u_int8_t width;
+} Tetris;
+
+void fillArray(Tetris* game) {
+    for (u_int8_t i = 0; i < game->height; i++) {
+        for (u_int8_t j = 0; j < game->width; j++) {
+            game->board[i][j] = 0;
+        }
+    }
+}
+
+void pru_int8_tArray(Tetris* game) {
+    for (u_int8_t i = 0; i < game->height; i++) {
+        for (u_int8_t j = 0; j < game->width; j++) {
+            printf("%d ",game->board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int checkLose(Tetris* game) {
+    for (int j = 0; j < game->width; j++) {
+        if (game->board[0][j] != 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int moov(u_int8_t btn, Tetris* game) {
+    u_int8_t lost = checkLose(game);
+    
+    return 0;
+}
+
+int main() {
+    
+    Tetris* game = malloc(sizeof(Tetris));
+    game->height = 7;
+    game->width = 5;
+    game->board = malloc(sizeof(u_int8_t*) * game->height);
+    for (u_int8_t i=0; i < game->height; i++) {
+        game->board[i] = malloc(sizeof(u_int8_t) * game->width);
+    }
+    
+    fillArray(game);
+    pru_int8_tArray(game);
+    
+    
     return 0;
 }
