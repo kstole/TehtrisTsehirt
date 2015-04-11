dc2726c376b6d70ca3ccb3e7febc9514b98dda67
Kyler Stole <kmstole@10-162-191-197.wireless.oregonstate.edu>
2015-04-10 22:30:32 -0700

    Initial commit of generated main.c

---
+++ TehtrisTsehirt/TehtrisTsehirt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TehtrisTsehirt
+//
+//  Created by Kyler Stole on 4/10/15.
+//  Copyright (c) 2015 Kyler Stole. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
