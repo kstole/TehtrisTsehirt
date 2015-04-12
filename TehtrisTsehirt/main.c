49718c5bab1847ed586cf9d3d6ae75712fd0703b
Kyler Stole <stolek@onid.oregonstate.edu>
2015-04-11 17:48:24 -0700

    Removed dynamic allocation of variables

--- TehtrisTsehirt/main.c
+++ TehtrisTsehirt/main.c
@@ -12,34 +12,51 @@
 #include <stdlib.h> //malloc
 #include <inttypes.h> //Zach's computer
 typedef uint8_t u_int8_t;
+#define W 8
+#define H 10
 
-typedef struct Tetris {
-    u_int8_t** board;
-    u_int8_t height;
-    u_int8_t width;
-    u_int8_t piece;
-    /* 1111 222 333 44  55 666 77
-     *        2 3   44 55   6   77 */
-    u_int8_t o; // orientation
-    /* 0 | upright
-     * 1 | right
-     * 2 | down
-     * 3 | left
-     */
-    u_int8_t x;
-    u_int8_t y;
-    
-} Tetris;
+/* Global Vars */
+u_int8_t board[W][H];
+u_int8_t piece;
+/* 1111 222 333 44  55 666 77
+ *        2 3   44 55   6   77 */
+u_int8_t o; // orientation
+/* 0 | upright
+ * 1 | right
+ * 2 | down
+ * 3 | left
+ */
+u_int8_t x;
+u_int8_t y;
+
+
+//typedef struct Tetris {
+//    u_int8_t** board;
+//    u_int8_t height;
+//    u_int8_t width;
+//    u_int8_t piece;
+//    /* 1111 222 333 44  55 666 77
+//     *        2 3   44 55   6   77 */
+//    u_int8_t o; // orientation
+//    /* 0 | upright
+//     * 1 | right
+//     * 2 | down
+//     * 3 | left
+//     */
+//    u_int8_t x;
+//    u_int8_t y;
+//    
+//} Tetris;
 
 /*
  * parameters: a pointer to a game of tetris
  * fills array with 0s
  */
 
-void fillArray(Tetris* game) {
-    for (u_int8_t i = 0; i < game->height; i++) {
-        for (u_int8_t j = 0; j < game->width; j++) {
-            game->board[i][j] = 0;
+void fillArray() {
+    for (u_int8_t i = 0; i < H; i++) {
+        for (u_int8_t j = 0; j < W; j++) {
+            board[i][j] = 0;
         }
     }
 }
@@ -50,10 +67,10 @@ void fillArray(Tetris* game) {
  * prints array as a matrix
  */
 
-void printArray(Tetris* game) {
-    for (u_int8_t i = 0; i < game->height; i++) {
-        for (u_int8_t j = 0; j < game->width; j++) {
-            printf("%d ",game->board[i][j]);
+void printArray() {
+    for (u_int8_t i = 0; i < H; i++) {
+        for (u_int8_t j = 0; j < W; j++) {
+            printf("%d ",board[i][j]);
         }
         printf("\n");
     }
@@ -65,9 +82,9 @@ void printArray(Tetris* game) {
  * checks top row for 1s
  */
 
-int checkLose(Tetris* game) {
-    for (int j = 0; j < game->width; j++) {
-        if (game->board[0][j] != 0) {
+int checkLose() {
+    for (int j = 0; j < W; j++) {
+        if (board[0][j] != 0) {
             return 1;
         }
     }
@@ -84,37 +101,23 @@ int genPiece() {
  * 0 if you should
  */
 
-int moov(u_int8_t btn, Tetris* game) {
-    u_int8_t lost = checkLose(game);
+int moov(u_int8_t btn) {
+    u_int8_t lost = checkLose();
     
-    if (game->piece == 0) game->piece = genPiece();
+    if (piece == 0) piece = genPiece();
     
     
     return 0;
 }
 
-Tetris* createGame( u_int8_t height, u_int8_t width)
-{
-   Tetris* game = malloc(sizeof(Tetris));
-   game->height = height;
-   game->width = width;
-   game->board = malloc(sizeof(u_int8_t*) * game->height);
-   for(u_int8_t i=0; i < game->height; i++)
-   {
-      game->board[i] = malloc(sizeof(u_int8_t) * game->width);
-   }
-   return game;
-}
-
 int main() {
     srand((unsigned int)time(NULL));
-    Tetris* game = createGame(7, 5);
-    fillArray(game);
-    while(checkLose(game) != 1) {
-        printArray(game);
+    fillArray();
+    while(checkLose() != 1) {
+        printArray();
         sleep(1);
-        game->board[2][2] = 4;
-        printArray(game);
+        board[2][2] = 4;
+        printArray();
         sleep(1);
     }
     return 0;
